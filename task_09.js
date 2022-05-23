992b13715803ca444c94d0dbd8f730d96652c329
K1lltaN <94398731+K1lltaN@users.noreply.github.com>
2022-05-23 14:22:56 +0300

    Create task_09.js

---
+++ task_09.js
@@ -0,0 +1,9 @@
+import users from "./users.js"
+
+const getNamesSortedByFriendsCount = users => {
+
+  return   users.sort((a , b) => a.friends.length - b.friends.length) .map((user) =>   user.name );  
+  
+};
+
+console.log(getNamesSortedByFriendsCount(users));
